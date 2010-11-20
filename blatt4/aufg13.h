29506e5c988ee1a1291c612e029cef22933aaf53
Matthias Rampke <matthias@rampke.de>
2010-11-20 18:55:44 +0100

    sort routine

--- blatt4/aufg13.h
+++ blatt4/aufg13.h
@@ -8,4 +8,49 @@ static void swap(float* f1, float* f2)
     *f1 = temp;
 }
 
+/* in-place quicksort */
+static void sort(float* feld, int N)
+{
+    /* Abbruchbedingung: Einelementige Listen sind sortiert */
+    if ( N <= 1 ) return;
+
+    int pivot = N/2;
+    int i = 0;
+    float temp;
+
+    /* Elemente, die kleiner als feld[pivot] sind dahinter verschieben */
+    while ( i < pivot )
+    {
+        if ( feld[i] < feld[pivot] )
+        {
+            temp = feld[i];
+            feld[i] = feld[pivot-1];
+            feld[pivot-1] = feld[pivot];
+            feld[pivot] = temp;
+            pivot--;
+        }
+        else i++;
+    }
+
+    /* Elemente, die größer als feld[pivot] sind davor verschieben */
+    while ( i < N )
+    {
+        if ( feld[i] > feld[pivot] )
+        {
+            temp = feld[i];
+            feld[i] = feld[pivot+1];
+            feld[pivot+1] = feld[pivot];
+            feld[pivot] = temp;
+            pivot++;
+        }
+        else i++;
+    }
+
+    /* Rekursion */
+    sort(feld, pivot);
+    sort(feld+pivot+1, N-pivot-1);
+
+    return;
+}
+
 #endif
