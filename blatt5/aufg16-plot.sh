1394bc96bc3e7bfb0105590e9f89284175e5577e
Matthias Rampke <matthias@rampke.de>
2010-11-29 11:12:23 +0100

    FIX use correct data file for fitting

--- blatt5/aufg16-plot.sh
+++ blatt5/aufg16-plot.sh
@@ -15,9 +15,9 @@ f_10(x)=(1+erf((x-x0_10)/(sqrt(2)*s_10)))/2
 f_100(x)=(1+erf((x-x0_100)/(sqrt(2)*s_100)))/2
 f_1000(x)=(1+erf((x-x0_1000)/(sqrt(2)*s_1000)))/2
 
-fit f_10(x) 'aufg16-vhq.txt' index 0 using 1:2 via x0_10,s_10
-fit f_100(x) 'aufg16-vhq.txt' index 1 using 1:2 via x0_100,s_100
-fit f_1000(x) 'aufg16-vhq.txt' index 2 using 1:2 via x0_1000,s_1000
+fit f_10(x) '$1' index 0 using 1:2 via x0_10,s_10
+fit f_100(x) '$1' index 1 using 1:2 via x0_100,s_100
+fit f_1000(x) '$1' index 2 using 1:2 via x0_1000,s_1000
 
 set samples 1000
 set key right bottom
