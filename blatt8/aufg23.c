ff35b848e9530f3cb41cc29e0e52a58f9fcd52c1
Matthias Rampke <matthias@rampke.de>
2010-12-16 12:18:52 +0100

    generalization

--- blatt8/aufg23.c
+++ blatt8/aufg23.c
@@ -3,11 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <assert.h>
 
 #define DIM 2
 #define BINS 100
 #define BURNIN 100000
-#define SAMPLES 100000000
+#define SAMPLES 10000000
+#define CHAINS 10
+
+#define N_LAMBDAS 4
+static const double LAMBDAS[N_LAMBDAS] = {1.5, 2, 5, 10};
+#define N_NS 4
+static const double NS[N_NS] = {1, 3, 10, 100};
+#define MAX_NS 100
 
 static double lambda;
 
@@ -58,36 +66,60 @@ void bin_density(const size_t N, const state_t* state, unsigned int rho[BINS])
 
 int main(int argc, char** argv)
 {
-    size_t i;
-    size_t N = 1*DIM;
+    size_t i,j,k,l;
+    size_t N;
     unsigned int* rho;  
     double* density;
     state_t* state;
 
     density = malloc(sizeof(double)*BINS);
     rho = malloc(sizeof(unsigned int)*BINS);
-    state = malloc(sizeof(state_t)*N);
-    for(i=0;i<BINS;i++)
+    state = malloc(sizeof(state_t)*MAX_NS*DIM);
+    assert(density!=NULL && rho!=NULL && state!=NULL);
+    for(j=0;j<N_NS;j++)
     {
-        rho[i]=0;
-        density[i] = 0;
-    }
+        N = NS[j]*DIM;
+        for(k=0;k<N_LAMBDAS;k++)
+        {
+            lambda = LAMBDAS[k] * pow(N/DIM,1./DIM);
 
-    lambda = 10;
+            printf("# N = %d, lambda = %f\n", (int)NS[j], LAMBDAS[k]);
+            fflush(stdout);
 
-    /* initial state */
-    for(i=0;i<N;i++)
-        state[i] = 0.5*(lambda-1)*((double)2*prng()/PRNG_MAX-1);
+            for(i=0;i<BINS;i++)
+            {
+                rho[i]=0;
+                density[i] = 0;
+            }
 
-    /* burn in */
-    for(i=0;i<BURNIN;i++)
-        metropolis_evolve_state(N, state, H);
+            for(l=0;l<CHAINS;l++)
+            {
+                /* initial state */
+                for(i=0;i<N;i++)
+                        state[i] = 0.5*(lambda-1)*((double)2*prng()/PRNG_MAX-1);
 
-    /* sampling */
-    for(i=0;i<SAMPLES;i++)
-    {
-        metropolis_evolve_state(N, state, H);
-        bin_density(N, state, rho);
+                /* burn in */
+                for(i=0;i<BURNIN;i++)
+                    metropolis_evolve_state(N, state, H);
+
+                /* sampling */
+                for(i=0;i<SAMPLES;i++)
+                {
+                    metropolis_evolve_state(N, state, H);
+                    bin_density(N, state, rho);
+                }
+            }
+
+            /* correct local density for size of (DIM-1)-sphere */
+            for(i=1;i<BINS;i++)
+                density[i] = (double)rho[i] * pow(((double)2*BINS/( CHAINS*SAMPLES )) / (i*lambda), DIM-1);
+
+            for(i=1;i<BINS;i++)
+                printf("%lf %lf\n", (double)i*lambda/(2*BINS), density[i]);
+
+            printf("\n\n");
+            fflush(stdout);
+        }
     }
 
     free(state);
